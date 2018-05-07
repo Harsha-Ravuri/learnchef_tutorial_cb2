6a8e3835f3d65e00d60c753944723db3f6ff97c5
harsharavuri <sreeharsharavuri@gmail.com>
2018-05-07 17:30:13 -0400

    learn chef tutorial - web server cb - 1st commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,26 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package_name =
+  service_name =
+    case node['platform']
+    when 'centos' then 'httpd'
+    when 'ubuntu' then 'apache2'
+    end
+
+# Install the package.
+package package_name
+
+# Start and enable the service.
+service service_name do
+  action [:enable, :start]
+end
+
+# Serve a custom home page.
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
